49748c40fd56765773016350bb21c21d0681b15c
Haider Hameed <haiderhameed@gmail.com>
2024-08-01 20:19:02 -0700

    feat: adding button from shadcn

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Button>Click me</Button>
+    </div>
     </>
   )
 }
