30e55b36ada1e83bfa81f475d25aac493686fb81
Haider Hameed <haiderhameed@gmail.com>
2024-08-02 16:09:50 -0700

    feat: added dummy leaderboard

--- packages/backend/src/trpc.ts
+++ packages/backend/src/trpc.ts
@@ -6,10 +6,16 @@ export const router = t.router;
 export const publicProcedure = t.procedure;
 
 export const appRouter = router({
-  userList: publicProcedure
+  leaderboard: publicProcedure
     .query(async () => {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      return [{ id: 1, name: 'Alice' }, { id: 2, name: 'John' }];
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      return [
+        { rank: 1, displayName: 'Alice Smith', username: '@alice', roi: 50 },
+        { rank: 2, displayName: 'Bob Johnson', username: '@bob', roi: 40 },
+        { rank: 3, displayName: 'Charlie Brown', username: '@charlie', roi: 30 },
+        { rank: 4, displayName: 'David Lee', username: '@david', roi: 20 },
+        { rank: 5, displayName: 'Eve Williams', username: '@eve2', roi: 10 },
+      ];
     }),
   createUser: publicProcedure
     .input(z.object({ name: z.string() }))
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import './globals.css'
-import { ComingSoon } from "./components/coming-soon"
+// import { ComingSoon } from "./components/coming-soon"
 import { ThemeProvider } from "@/components/theme-provider"
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { httpBatchLink } from '@trpc/client';
 import { useState } from 'react';
 import { trpc } from './trpc';
+import { Leaderboard } from './components/leaderboard';
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
@@ -29,7 +30,8 @@ function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <ComingSoon />
+          {/* <ComingSoon /> */}
+          <Leaderboard />
         </ThemeProvider>
       </QueryClientProvider>
     </trpc.Provider>
