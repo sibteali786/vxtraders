2646b181b96cdee4df716149b181c8729e2f7109
Haider Hameed <haiderhameed@gmail.com>
2024-08-01 21:04:08 -0700

    feat: enabling dark mode as default

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
 import './globals.css'
 import { ComingSoon } from "./components/coming-soon"
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <ComingSoon />
-    </>
+    </ThemeProvider>
   )
 }
 
