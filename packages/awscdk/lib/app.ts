e5bdc25dd1e14f2ca9238359f88e262e4a975ca6
Haider Hameed <haiderhameed@gmail.com>
2024-08-01 19:54:13 -0700

    adding website stack

--- packages/awscdk/lib/app.ts
+++ packages/awscdk/lib/app.ts
@@ -1,9 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { CronStack } from "./stacks/cron-stack";
 import { Configuration } from "./config";
+import { WebsiteStack } from "./stacks/website-stack";
 
 export class VxTradersCdkApp extends cdk.App {
   readonly cronStack: CronStack;
+  readonly websiteStack: WebsiteStack;
 
   constructor(props: cdk.AppProps) {
     super(props);
@@ -14,5 +16,6 @@ export class VxTradersCdkApp extends cdk.App {
 
     const stage = Configuration.getAppConfig().env.STAGE;
     this.cronStack = new CronStack(this, `CronStack-${stage}`, stackProps);
+    this.websiteStack = new WebsiteStack(this, `WebsiteStack-${stage}`, stackProps);
   }
 }
---
+++ packages/awscdk/lib/stacks/website-stack.ts
@@ -0,0 +1,40 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import { Configuration } from "../config";
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+
+export class WebsiteStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    const stage = Configuration.getAppConfig().env.STAGE;
+
+    const websiteBucket = new s3.Bucket(this, `WebsiteBucket-${stage}`, {
+      bucketName: `vxtraders-website-bucket-${stage}`,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html",
+    });
+
+    // const distribution = new cloudfront.Distribution(this, `WebsiteDistribution-${stage}`, {
+    //   defaultBehavior: {
+    //     origin: new origins.S3Origin(websiteBucket),
+    //     viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    //   },
+    // });
+
+    new s3Deployment.BucketDeployment(this, `DeployWebsite-${stage}`, {
+      sources: [s3Deployment.Source.asset("../frontend/dist")],
+      destinationBucket: websiteBucket,
+      // distribution,
+      // distributionPaths: ['/*'],
+    });
+
+    new cdk.CfnOutput(this, "WebsiteURL", {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: "URL of the website",
+    });
+  }
+}
