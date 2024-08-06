7933ebce57697f236b6b784666fdb15eaf1037c8
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-06 16:00:01 +0500

    refactor: adjusted size of skeleton for mobile

--- packages/frontend/src/components/common/preview-skeleton.tsx
+++ packages/frontend/src/components/common/preview-skeleton.tsx
@@ -5,10 +5,10 @@ import { Skeleton } from "../ui/skeleton";
 export function PreviewSkeleton() {
   return (
     <Card className="flex items-center space-x-4 p-3">
-      <Skeleton className="h-12 w-12 rounded-full" />
+      <Skeleton className="h-8 w-8 rounded-full" />
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-3 w-[250px]" />
+        <Skeleton className="h-3 w-[200px]" />
       </div>
     </Card>
   );
