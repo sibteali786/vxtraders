154aae8b451e8d25c802f152f74c51c1c10250c8
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-07 13:40:38 +0500

    style: navbar styles updated

--- packages/frontend/src/components/horizontal-menu.tsx
+++ packages/frontend/src/components/horizontal-menu.tsx
@@ -2,30 +2,26 @@ import { NavLink } from "react-router-dom";
 import { cva } from "class-variance-authority";
 import { ShuffleIcon, PieChartIcon, TrophyIcon, SettingsIcon, CircleHelpIcon } from "./Icons/Icons";
 const navLinkClasses = cva(
-  "flex flex-col items-center justify-center gap-1 text-xs font-medium sm:text-sm",
+  "flex flex-col items-center justify-center gap-[3px] text-xs font-medium",
   {
     variants: {
       active: {
         true: "text-special",
         false: "text-muted-foreground hover:text-muted-foreground/80",
       },
-      mainNav: {
-        true: "relative -top-[30px] bg-white p-6 rounded-full",
-      },
     },
     defaultVariants: {
       active: false,
-      mainNav: false,
     },
   },
 );
 
 // responsible for icon size in menu
-const iconClasses = cva(["h-7", "w-7"]);
+const iconClasses = cva(["h-5", "w-5"]);
 
 export function HorizontalMenu() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-background w-full flex justify-center">
+    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-background w-full flex justify-center h-[70px]">
       <div className="flex justify-around max-w-[600px] border-t border-border w-full">
         <NavLink
           to="/select-asset"
@@ -41,10 +37,11 @@ export function HorizontalMenu() {
         <NavLink
           to="/"
           className={({ isActive }) => {
-            return navLinkClasses({ active: isActive, mainNav: true });
+            return navLinkClasses({ active: isActive });
           }}
         >
-          <TrophyIcon className="h-8 w-8" />
+          <TrophyIcon />
+          Leaderboard
         </NavLink>
         <NavLink to="/settings" className={({ isActive }) => navLinkClasses({ active: isActive })}>
           <SettingsIcon className={iconClasses} />
