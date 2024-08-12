281b7aebb6173a3f6786e8c66a688044a91cab35
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-12 16:54:36 +0500

    style: updates for portfolio screen

--- packages/frontend/src/components/common/Position/tradeSummary.tsx
+++ packages/frontend/src/components/common/Position/tradeSummary.tsx
@@ -4,10 +4,15 @@ import { InfoBlock } from "../infoBlock";
 export function TradeSummary() {
   return (
     <div className="p-4 grid grid-cols-3 gap-y-6">
-      <InfoBlock label="Size" value={0.0157} />
+      <InfoBlock label="Value" value="$0.0157" />
       <InfoBlock label="Entry Price" value="$58,531" />
-      <InfoBlock label="Liquidation Price" value="$40,000" />
-      <InfoBlock label="Solana" src="/solana.png" />
+      <InfoBlock label="Liqd. Price" value="$40,000" />
+      <InfoBlock label="Asset" src="/solana.png">
+        <div className="flex items-center gap-1">
+          <img src="/solana.png" />
+          <p>Solana</p>
+        </div>
+      </InfoBlock>
       <InfoBlock label="Leverage">
         <div className="flex gap-2">
           <p> 10x</p>
--- packages/frontend/src/components/common/Position/tradesTimeline.tsx
+++ packages/frontend/src/components/common/Position/tradesTimeline.tsx
@@ -1,85 +1,69 @@
 import { Card } from "@/components/ui/card";
 
+const tradesHistory = [
+  {
+    type: "Opened",
+    date: "7 days ago",
+    amount: "$56,234",
+    leverage: "10x",
+  },
+  {
+    type: "Closed",
+    date: "3 days ago",
+    amount: "$23,252",
+    percentage: -10,
+  },
+  {
+    type: "Closed",
+    date: "June 02, 2024",
+    amount: "$23,252",
+    percentage: +50,
+  },
+  {
+    type: "Closed",
+    date: "June 02, 2024",
+    amount: "$23,252",
+    percentage: +50,
+  },
+];
+
 export function TradesTimeline() {
   return (
     <div className="relative pr-default">
       {/* Timeline line */}
-      <div className="absolute left-5 top-[50px] bottom-[40px] w-0.5 bg-white opacity-50" />
+      <div className="absolute left-5 top-[50px] bottom-[30px] w-0.5 bg-muted opacity-50" />
 
       <div className="space-y-6 pl-12">
         {/* First event */}
-        <div className="relative">
-          {/* Circle Indicator */}
-          <div className="absolute left-[-32px] top-[40px] w-3 h-3 rounded-full bg-white" />
+        {tradesHistory
+          ? tradesHistory.map((trade, index) => (
+              <div className="relative" key={index}>
+                {/* Circle Indicator */}
+                <div className="absolute left-[-33px] top-[40px] w-3 h-3 rounded-full bg-[#605F5F]" />
 
-          <Card className="p-3 bg-gray-800 rounded-lg">
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-sm font-semibold text-purple-300">Opened</p>
-                <p className="text-sm text-gray-400">7 days ago</p>
+                <Card className="px-4 py-4 rounded-lg">
+                  <div className="flex justify-between items-center">
+                    <div className="flex flex-col gap-[1px]">
+                      <p className="text-base font-semibold">{trade.type}</p>
+                      <p className="text-sm text-gray-400">{trade.date}</p>
+                    </div>
+                    <div className="text-right">
+                      <p className="text-base font-semibold">{trade.amount}</p>
+                      {trade.leverage ? (
+                        <p className="text-sm text-gray-400">{trade.leverage}</p>
+                      ) : (
+                        <p
+                          className={`text-sm ${trade?.percentage && trade.percentage > 0 ? `text-green-500` : "text-red-500"}`}
+                        >
+                          {trade.percentage}%
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                </Card>
               </div>
-              <div className="text-right">
-                <p className="text-lg font-semibold text-purple-300">$56,234</p>
-                <p className="text-sm text-gray-400">10x</p>
-              </div>
-            </div>
-          </Card>
-        </div>
-
-        {/* Second event */}
-        <div className="relative">
-          {/* Circle Indicator */}
-          <div className="absolute left-[-32px] top-[30px] w-3 h-3 rounded-full bg-white" />
-
-          <Card className="p-3 rounded-lg border border-gray-600">
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-sm font-semibold text-white">Closed</p>
-                <p className="text-sm text-gray-400">3 days ago</p>
-              </div>
-              <div className="text-right">
-                <p className="text-lg font-semibold text-purple-300">$23,252</p>
-                <p className="text-sm text-red-500">-10%</p>
-              </div>
-            </div>
-          </Card>
-        </div>
-
-        {/* Third event */}
-        <div className="relative">
-          {/* Circle Indicator */}
-          <div className="absolute left-[-32px] top-[30px] w-3 h-3 rounded-full bg-white" />
-
-          <Card className="p-3 rounded-lg border border-gray-600">
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-sm font-semibold text-white">Closed</p>
-                <p className="text-sm text-gray-400">June 02, 2024</p>
-              </div>
-              <div className="text-right">
-                <p className="text-lg font-semibold text-purple-300">$23,252</p>
-                <p className="text-sm text-green-500">+50%</p>
-              </div>
-            </div>
-          </Card>
-        </div>
-        <div className="relative">
-          {/* Circle Indicator */}
-          <div className="absolute left-[-32px] top-[30px] w-3 h-3 rounded-full bg-white" />
-
-          <Card className="p-3 rounded-lg border border-gray-600">
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-sm font-semibold text-white">Closed</p>
-                <p className="text-sm text-gray-400">June 02, 2024</p>
-              </div>
-              <div className="text-right">
-                <p className="text-lg font-semibold text-purple-300">$23,252</p>
-                <p className="text-sm text-green-500">+50%</p>
-              </div>
-            </div>
-          </Card>
-        </div>
+            ))
+          : null}
       </div>
     </div>
   );
