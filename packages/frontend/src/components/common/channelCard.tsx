6ca0cd2d7a3aa3ed524302b04237f0f19debd8cc
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-13 22:13:52 +0500

    style: channel card consistent styles

--- packages/frontend/src/components/common/channelCard.tsx
+++ packages/frontend/src/components/common/channelCard.tsx
@@ -4,7 +4,7 @@ import { Channel } from "@vxtraders/shared";
 
 const ChannelCard = (props: { channel: Channel }) => {
   return (
-    <Card className="flex items-center py-3 px-2 mb-4  mobile-small:px-2 mobile-medium:px-4">
+    <Card className="flex items-center bg-transparent py-3 px-2 mb-4  mobile-small:px-2 mobile-medium:px-4">
       <Avatar className="mr-4 w-8 h-8 mobile-medium:w-9 mobile-medium:h-9">
         <AvatarImage src={props.channel.avatar} alt={props.channel.name} />
         <AvatarFallback>{props.channel.name.charAt(0)}</AvatarFallback>
