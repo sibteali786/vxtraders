39e1c9c77f0d81c56734f473e5462e337cdfbbcf
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-13 22:15:43 +0500

    refactor: added link to portfolio

--- packages/frontend/src/components/common/traderCard.tsx
+++ packages/frontend/src/components/common/traderCard.tsx
@@ -1,27 +1,30 @@
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Card } from "../ui/card";
 import { TraderSummary } from "@vxtraders/shared";
+import { Link } from "react-router-dom";
 
 export function TraderPreview(props: { trader: TraderSummary }) {
   return (
-    <Card className="flex items-center py-3 px-2 mobile-medium:px-4">
-      <Avatar className="mr-4 w-10 h-10 mobile-medium:w-9 mobile-medium:h-9">
-        <AvatarImage src="/avatar.png" alt="Maxine Shu" />
-        <AvatarFallback>MS</AvatarFallback>
-      </Avatar>
-      <div className="flex-1 flex flex-col gap-1">
-        <p className="font-semibold text-sm mobile-medium:text-base leading-[14px]">
-          {props.trader.displayName}
-        </p>
-        <p className="text-muted-foreground text-xs mobile-medium:text-sm leading-[14px]">
-          {props.trader.username}
-        </p>
-      </div>
-      <div className="flex items-center">
-        <p className="font-semibold text-success mr-2 text-sm mobile-medium:text-base">
-          +{props.trader.roi}%
-        </p>
-      </div>
-    </Card>
+    <Link to={`/portfolio/${props.trader.id}`}>
+      <Card className="flex items-center py-3 px-2 mobile-medium:px-4">
+        <Avatar className="mr-4 w-10 h-10 mobile-medium:w-9 mobile-medium:h-9">
+          <AvatarImage src="/avatar.png" alt="Maxine Shu" />
+          <AvatarFallback>MS</AvatarFallback>
+        </Avatar>
+        <div className="flex-1 flex flex-col gap-1">
+          <p className="font-semibold text-sm mobile-medium:text-base leading-[14px]">
+            {props.trader.displayName}
+          </p>
+          <p className="text-muted-foreground text-xs mobile-medium:text-sm leading-[14px]">
+            {props.trader.username}
+          </p>
+        </div>
+        <div className="flex items-center">
+          <p className="font-semibold text-success mr-2 text-sm mobile-medium:text-base">
+            +{props.trader.roi}%
+          </p>
+        </div>
+      </Card>
+    </Link>
   );
 }
