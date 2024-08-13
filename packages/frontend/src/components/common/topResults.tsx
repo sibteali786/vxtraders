87b759f322a332f04b81ac60cbda0bac75468629
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-13 22:14:54 +0500

    refactor: updates to Top 1% styles

--- packages/frontend/src/components/common/topResults.tsx
+++ packages/frontend/src/components/common/topResults.tsx
@@ -9,8 +9,8 @@ interface TopResultsProps extends React.HTMLAttributes<HTMLDivElement> {
 export const TopResults: React.FC<TopResultsProps> = ({ className, label, value, ...props }) => {
   return (
     <div className={`text-right ${className}`} {...props}>
+      <p className="text-xs text-muted-foreground font-bold mb-[-2px] tracking-widest">{label}</p>
       <p className="text-purple-500 text-lg font-bold">{value}</p>
-      <p className="text-xs text-muted-foreground">{label}</p>
     </div>
   );
 };
