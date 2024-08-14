f64b7aebf9536fffec7841a17c8723f0c88ab908
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-14 18:59:50 +0500

    refactor: redesign place order

--- packages/frontend/src/pages/trade/placeOrder.tsx
+++ packages/frontend/src/pages/trade/placeOrder.tsx
@@ -1,98 +1,87 @@
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 import { Slider } from "@/components/ui/slider";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { FaBitcoin } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Bitcoin, TrendingDown, TrendingUp } from "lucide-react";
+import { Link, useParams } from "react-router-dom";
+import { MainHeading } from "@/components/common/mainHeading";
+import { Card } from "@/components/ui/card";
 
 export function PlaceVirtualOrder() {
-  const [orderSize, setOrderSize] = useState(7);
-  const [leverage, setLeverage] = useState(5);
-  const [mode, setMode] = useState("short");
+  const [mode, setMode] = useState<"long" | "short">("long");
+  const [orderSize, setOrderSize] = useState(50); // Default to 50%
+  const [leverage, setLeverage] = useState(5); // Default to 5x
+  const [price] = useState(56715); // Example market price
+  const [balance] = useState(10000); // Example balance
 
+  const liqPrice = leverage === 1 ? "N/A" : `$${(price * (1 - 1 / leverage)).toFixed(2)}`;
+  const orderValue = `$${((balance * orderSize * leverage) / 100).toFixed(2)}`;
+  const { assetName } = useParams<{ assetName: string }>();
   return (
-    <div className="p-6 space-y-6">
-      <h1 className="text-2xl mobile-medium:text-3xl font-bold">Place a Virtual Order</h1>
-
-      <div className="space-y-4">
-        <div className="flex justify-between items-center">
-          <Label>Selected Asset</Label>
-          <div className="flex items-center space-x-2">
-            <FaBitcoin className="text-2xl text-orange-500" />
-            <span className="font-semibold">Bitcoin</span>
+    <div className="px-default flex flex-col gap-4">
+      <MainHeading title="Place a Virtual Order" />
+      <p className="text-sm text-right">{`1 BTC = $${price.toLocaleString()}`}</p>
+      <Card className="flex flex-col gap-6 p-4">
+        <div className="space-y-4">
+          <Tabs
+            value={mode}
+            onValueChange={(value) => setMode((value as unknown as "long") || "short")}
+          >
+            <TabsList className="grid grid-cols-2 gap-2">
+              <TabsTrigger value="long" className="flex gap-2 text-base">
+                <TrendingUp size={20} /> Long{" "}
+              </TabsTrigger>
+              <TabsTrigger value="short" className="flex gap-2 text-base">
+                <TrendingDown size={20} />
+                Short
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
+          <div className="flex justify-between">
+            <label className="block text-sm font-medium">Order Size ({orderSize}%)</label>
+            <div className="text-sm">${((balance * orderSize) / 100).toFixed(2)}</div>
           </div>
         </div>
-
-        <div className="flex justify-between items-center">
-          <Label>Market Price</Label>
-          <span className="font-semibold">$53,123</span>
+        <Slider
+          value={[orderSize]}
+          onValueChange={(value) => setOrderSize(value[0])}
+          min={1}
+          max={100}
+          className="w-full"
+        />
+        <div className="flex justify-between">
+          <label className="block text-sm font-medium">Leverage</label>
+          <div className="text-sm">{leverage}x</div>
         </div>
 
-        <div className="flex justify-between items-center">
-          <Label>Virtual Balance</Label>
-          <span className="font-semibold">$10,000</span>
+        <Slider
+          value={[leverage]}
+          onValueChange={(value) => setLeverage(value[0])}
+          min={1}
+          max={20}
+          className="w-full"
+        />
+        <Link to="/position" className="my-4">
+          <Button className="w-full">
+            {mode === "long" ? "Long" : "Short"} {assetName}{" "}
+            <Bitcoin className="text-[#f7931a] w-4 h-4" />
+          </Button>
+        </Link>
+      </Card>
+      <div className="text-sm space-y-1 mt-2">
+        <div className="flex justify-between">
+          <span className="font-medium">Virtual Balance: </span>
+          <p className="text-sm font-bold">${balance.toLocaleString()}</p>
         </div>
-      </div>
-
-      <div className="space-y-4">
-        <Label className="font-bold text-base mobile-medium:text-lg">Mode</Label>
-        <RadioGroup
-          className="flex justify-around mobile-medium:justify-evenly space-x-6"
-          value={mode}
-          onValueChange={(value) => setMode(value)}
-        >
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="long" id="long" />
-            <Label htmlFor="long">Long</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="short" id="short" />
-            <Label htmlFor="short">Short</Label>
-          </div>
-        </RadioGroup>
-      </div>
-      <div className="space-y-4">
-        <Label>Order Size ({orderSize}%)</Label>
-        <div className="flex justify-between items-center">
-          <Slider
-            value={[orderSize]}
-            onValueChange={(value) => setOrderSize(value[0])}
-            className="w-full"
-            max={100}
-          />
-          <span className="ml-4">${((orderSize / 100) * 10000).toFixed(0)}</span>
+        <div className="flex justify-between">
+          <span className="font-medium">Liquidation Price: </span>
+          <p className="text-sm font-bold">{liqPrice}</p>
         </div>
-      </div>
-
-      <div className="space-y-4">
-        <Label>Leverage</Label>
-        <div className="flex justify-between items-center">
-          <Slider
-            value={[leverage]}
-            onValueChange={(value) => setLeverage(value[0])}
-            className="w-full"
-            min={1}
-            max={10}
-          />
-          <span className="ml-4">{leverage}x</span>
+        <div className="flex justify-between">
+          <span className="font-medium">Order Value: </span>
+          <p className="text-sm font-bold">{orderValue}</p>
         </div>
       </div>
-
-      <div className="space-y-4">
-        <div className="flex justify-between items-center">
-          <Label>Liquidation Price</Label>
-          <span className="font-semibold">$40,000</span>
-        </div>
-
-        <div className="flex justify-between items-center">
-          <Label>Order Value</Label>
-          <span className="font-semibold">$35,000</span>
-        </div>
-      </div>
-      <Link to='/position'>
-        <Button className="w-full mt-6">Place Order</Button>
-      </Link>
     </div>
   );
 }
