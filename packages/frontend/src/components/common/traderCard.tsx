5182c60994c44572558cc510811d903fb98292ad
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-17 16:49:06 +0500

    refactor: handle negative values as well

--- packages/frontend/src/components/common/traderCard.tsx
+++ packages/frontend/src/components/common/traderCard.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export function TraderPreview(props: { trader: TraderSummary }) {
   return (
     <Link to={`/portfolio/${props.trader.id}`}>
-      <Card className=" flex items-center py-3 px-2 mobile-medium:px-4 bg-black text-white "> 
+      <Card className=" flex items-center py-3 px-2 mobile-medium:px-4 bg-black text-white ">
         <Avatar className="mr-4 w-10 h-10 mobile-medium:w-9 mobile-medium:h-9">
           <AvatarImage src="/avatar.png" alt="Maxine Shu" />
           <AvatarFallback>MS</AvatarFallback>
@@ -20,8 +20,10 @@ export function TraderPreview(props: { trader: TraderSummary }) {
           </p>
         </div>
         <div className="flex items-center">
-          <p className="font-semibold text-success mr-2 text-sm mobile-medium:text-base">
-            +{props.trader.roi}%
+          <p
+            className={`font-semibold mr-2 text-sm mobile-medium:text-base ${props.trader.roi >= 0 ? "text-success" : "text-red-500"}`}
+          >
+            {props.trader.roi}%
           </p>
         </div>
       </Card>
