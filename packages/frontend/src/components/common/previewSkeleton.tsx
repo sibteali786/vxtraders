00008573d1d4ad55c000f9c8ff30b3320c265ad1
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-17 16:48:32 +0500

    refactor: fix size issues on mobile

--- packages/frontend/src/components/common/previewSkeleton.tsx
+++ packages/frontend/src/components/common/previewSkeleton.tsx
@@ -1,14 +1,13 @@
 import { Card } from "../ui/card";
 import { Skeleton } from "../ui/skeleton";
 
-
 export function PreviewSkeleton() {
   return (
     <Card className="flex items-center space-x-4 p-3">
-      <Skeleton className="h-8 w-8 rounded-full" />
-      <div className="space-y-2">
-        <Skeleton className="h-3 w-[250px]" />
-        <Skeleton className="h-3 w-[200px]" />
+      <Skeleton className="h-8 w-8 mobile-small:w-10 mobile-medium:w-9 rounded-full" />
+      <div className="space-y-2 w-full">
+        <Skeleton className="h-3 w-[70%] " />
+	<Skeleton className="h-3 w-[50%]" />
       </div>
     </Card>
   );
