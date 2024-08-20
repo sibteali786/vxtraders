97c1d63f1334dc5983921984344fc6943a018a20
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-20 15:46:29 +0500

    fix: handle edge case

--- packages/frontend/src/lib/utils.ts
+++ packages/frontend/src/lib/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function formatCurrency(value: number | string) {
+export function formatCurrency(value: number | string | undefined) {
+  if (!value) return "$0";
   if (value === 0) return "$0";
   if (typeof value === "string") value = Number(value);
   const formatter = new Intl.NumberFormat("en-US", {
