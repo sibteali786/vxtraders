f5b87b22c61c9da98a06dc9e2c3efa3a21745e1b
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-23 12:10:15 +0500

    refactor: updated styles for error component

--- packages/frontend/src/components/common/Error/Error.tsx
+++ packages/frontend/src/components/common/Error/Error.tsx
@@ -1,40 +1,33 @@
-import { CircleX } from "lucide-react";
 import { motion } from "framer-motion";
 
 interface ErrorProps {
   children: React.ReactNode;
+  title: string;
+  buttonText?: string;
+  onClick?: () => void;
 }
 
-export const Error: React.FC<ErrorProps> = ({ children }) => {
+export const Error: React.FC<ErrorProps> = ({ children, title, buttonText, onClick }) => {
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
-      className="flex flex-col justify-center items-center w-full space-y-6 min-h-screen mb-6"
+      className="flex flex-col justify-center items-center w-full gap-2 my-auto"
     >
-      <motion.div
-        initial={{ scale: 1 }}
-        animate={{ scale: 1.1 }}
-        transition={{
-          repeat: Infinity,
-          repeatType: "reverse",
-          duration: 0.8,
-        }}
-      >
-        <CircleX className="text-destructive drop-shadow-lg" size={50} />
-      </motion.div>
-      <div className="mobile-medium:w-[80%] text-xl font-bold text-center text-white">
-        {children}
-      </div>
-      <motion.button
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
-        className="px-6 py-2 mt-4 bg-destructive text-white font-semibold rounded-lg shadow-md hover:bg-red-700"
-        onClick={() => window.location.reload()} // Example action: Refresh the page
-      >
-        Retry
-      </motion.button>
+      <img src="/errorIcon.png" className='w-8' />
+      <h2 className="text-xl font-bold text-center text-white mb-1 tracking-[4%]">{title}</h2>
+      <div className="text-sm text-center text-white">{children}</div>
+      {buttonText && onClick && (
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.95 }}
+          className="px-12 py-2 mt-4 bg-destructive text-white font-semibold rounded-lg shadow-md hover:bg-destructive-foreground"
+          onClick={onClick} // Example action: Refresh the page
+        >
+          {buttonText}
+        </motion.button>
+      )}
     </motion.div>
   );
 };
