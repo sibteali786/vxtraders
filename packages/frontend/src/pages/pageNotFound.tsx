470b17ef006eada1109d15df2c485d1283a30a94
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-26 11:54:07 +0500

    fix: page not found aligning

--- packages/frontend/src/pages/pageNotFound.tsx
+++ packages/frontend/src/pages/pageNotFound.tsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 export function PageNotFound() {
   return (
-    <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-md text-center">
+    <div className="flex flex-col items-center justify-center px-4 py-12 sm:px-6 lg:px-8 h-full">
+      <div className="max-w-md text-center ">
         <img
-          className="mx-auto h-[270px] w-[237px] text-muted-foreground"
-          src="/404 not found.png"
+          className="mx-auto text-muted-foreground mb-2"
+          src="/NotFound.png"
         />
-        <h1 className="mt-8 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
+        <h1 className="text-xl font-bold tracking-tight text-foreground sm:text-4xl ">
           Page Not Found
         </h1>
-        <p className="mt-4 text-muted-foreground">
+        <p className="mt-2 text-muted-foreground text-sm">
           Sorry, the page you're looking for doesn't exist or has been moved.
         </p>
-        <div className="mt-6">
+        <div className="mt-4">
           <Button asChild>
             <Link to="/">View Leaderboards</Link>
           </Button>
