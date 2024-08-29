cde3bcb53e92ffde046618899e011d9a876c5e13
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-29 18:06:48 +0500

    fix: ngrok server not working fix

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl(), react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -15,4 +15,7 @@ export default defineConfig({
     outDir: "./docs", // Output directory for GitHub Pages
   },
   base: "./", // Base path for GitHub Pages
+  server: {
+    host: "127.0.0.1",
+  },
 });
