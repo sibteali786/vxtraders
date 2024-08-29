3cbad9fb2d9b3f33ceb5f3ef3a327946d810cced
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-29 16:32:23 +0500

    deployment stuff

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,13 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [basicSsl(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: "./docs", // Output directory for GitHub Pages
+  },
+  base: "./", // Base path for GitHub Pages
+});
