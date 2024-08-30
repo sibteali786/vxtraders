d68e759e5038df54778f9dd3428cc0416f49a2a9
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-08-30 18:22:18 +0500

    ran npx shadcn-ui@latest add tooltip

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,11 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
